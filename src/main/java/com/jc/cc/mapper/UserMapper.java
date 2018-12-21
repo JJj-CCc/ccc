261f9e6f376effd2cd6bed9867064b7ea3880031
jc <jiachen@autohome.com.cn>
2018-12-21 11:28:14 +0800

    test s
    Signed-off-by: jc <jiachen@autohome.com.cn>

--- src/main/java/com/jc/cc/mapper/UserMapper.java
+++ src/main/java/com/jc/cc/mapper/UserMapper.java
@@ -1,5 +1,5 @@
 package com.jc.cc.mapper;
-
+//sssss
 import com.jc.cc.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,3 +11,5 @@ public interface UserMapper {
     @Select("select * from user where id= #{id}")
         User getUserById(@Param("id") Integer id);
         }
+        @Insert("insert into user ")
+        void insertuser(@Param"username" String username,String sex);
